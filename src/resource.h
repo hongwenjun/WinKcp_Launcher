0e5ceebdfd787edc3f770466c96a4bda2d321057
蘭雅sRGB <hongwenjun@gmail.com>
2018-10-21 16:49:52 +0800

    Add files via upload

---
+++ src/resource.h
@@ -0,0 +1,8 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define DLG_MAIN                                100
+#define IDI_ICON1                               103
+#define OPEN_KCP                                40000
+#define CLOSE_KCP                               40001
