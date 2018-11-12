a93f8f59ea995ba60340f8c9994733370dc098aa
蘭雅sRGB <hongwenjun@gmail.com>
2018-11-12 23:14:48 +0800

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,26 @@ int hide_run_cmd(char* cmdline);
 bool open_kcp_server(void);
 bool open_kcp_udp_server(void);
 bool close_kcp_server(void);
+bool open_wireguard_udp2raw(void);
+
+void App_Initdialog(HWND & hwnd);
+
+HBITMAP g_hBitmap_DONATE; // 打赏图片的句柄
+HICON   g_hIcon;    // 对话框图标句柄
 
 HINSTANCE hInst;
 
+
 BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+
     switch (uMsg) {
+
     case WM_INITDIALOG: {
+            App_Initdialog(hwndDlg); // 设置标题栏图标,// 设置图片
+
         }
-        return TRUE;
+        break;
 
     case WM_CLOSE: {
             EndDialog(hwndDlg, 0);
@@ -38,6 +49,12 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 }
                 break;
 
+            case OPEN_UDP2RAW: {
+                    open_wireguard_udp2raw();
+                    EndDialog(hwndDlg, 0);
+                }
+                break;
+
             case CLOSE_KCP: {
                     close_kcp_server();
                     EndDialog(hwndDlg, 0);
@@ -53,10 +70,15 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 }
 
 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
     hInst = hInstance;
-    InitCommonControls();
+ //   InitCommonControls();
+
+    // 从资源中加载BMP文件和图标，这些工作也可以在WM_INITDIALOG消息中进行
+    g_hBitmap_DONATE = ::LoadBitmap(hInst, (LPCTSTR)IDB_BITMAP1);
+    g_hIcon = ::LoadIcon(hInst, (LPCTSTR)IDI_ICON1);
+
     return DialogBox(hInst, MAKEINTRESOURCE(DLG_MAIN), NULL, (DLGPROC)DlgMain);
 }
 
@@ -76,6 +98,15 @@ bool open_kcp_udp_server(void)
 
     return true;
 }
+
+bool open_wireguard_udp2raw(void)
+{
+    char cmdline[] = "cmd /c Windows_UDP2RAW.cmd";
+    hide_run_cmd(cmdline);
+
+    return true;
+}
+
 
 bool close_kcp_server(void)
 {
@@ -115,4 +146,17 @@ int hide_run_cmd(char* cmdline)
 //    CloseHandle(pi.hProcess);
 //    CloseHandle(pi.hThread);
     return ret;
+}
+
+
+void App_Initdialog(HWND & hwnd)
+{
+
+    // 设置标题栏图标
+    ::SendMessage(hwnd, WM_SETICON, IDI_ICON1, (long)g_hIcon);
+
+    // 设置图片
+    ::SendDlgItemMessage(hwnd, DONATE_PIC, STM_SETIMAGE, IMAGE_BITMAP, (long)g_hBitmap_DONATE);
+
+
 }
--- src/resource.h
+++ src/resource.h
@@ -4,6 +4,9 @@
 
 #define DLG_MAIN                                100
 #define IDI_ICON1                               103
+#define IDB_BITMAP1                             106
 #define OPEN_KCP                                40000
 #define CLOSE_KCP                               40001
 #define OPEN_KCP_UDP                            40003
+#define OPEN_UDP2RAW                            40004
+#define DONATE_PIC                              40005
