f364ad12dadb714fedf7f495b5ae70498aa66cb9
蘭雅sRGB <hongwenjun@gmail.com>
2018-11-15 00:03:13 +0800

    Update Windows_UDP2RAW.cmd

--- Windows_UDP2RAW.cmd
+++ Windows_UDP2RAW.cmd
@@ -3,7 +3,7 @@
 
 @set SERVER_IP=144.202.95.95
 
+@route add %SERVER_IP% mask 255.255.255.0 default METRIC default IF default
+
 @start /b udp2raw -c -r%SERVER_IP%:8855 -l0.0.0.0:9009 -k"passwd" --raw-mode faketcp
 
-
-start /b  C:\soft\TunSafe\TunSafe.exe
