a844377013b344beccec19c1b0d71189ebd1ae4b
蘭雅sRGB <hongwenjun@gmail.com>
2018-11-16 10:20:17 +0800

    Update wg_udp2raw.sh

--- wg_udp2raw.sh
+++ wg_udp2raw.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-#下载UDP2RAW
+# 修改ss-server密码，SS默认不能直接访问，修改第26行  ss-server -s 0.0.0.0 -p 40000
+PASSWORD=srgb.xyz
+
+# 下载UDP2RAW
 wget https://github.com/wangyu-/udp2raw-tunnel/releases/download/20181113.0/udp2raw_binaries.tar.gz
 tar xf udp2raw_binaries.tar.gz
 mv udp2raw_amd64 /usr/bin/udp2raw
@@ -19,7 +22,13 @@ cat <<EOF >/etc/rc.local
 #
 # rc.local
 
-udp2raw -s -l0.0.0.0:8866 -r 127.0.0.1:9009 -k "passwd" --raw-mode faketcp  >> /var/log/wg_udp2raw_.log &
+#  SS+KCP+UDP2RAW 加速  端口  8855
+# ss-server -s 127.0.0.1 -p 40000 -k ${PASSWORD} -m aes-256-gcm -t 300 >> /var/log/ss-server.log &
+# kcp-server -t "127.0.0.1:40000" -l ":4000" -mode fast2 -mtu 1300  >> /var/log/kcp-server.log &
+# udp2raw -s -l0.0.0.0:8855 -r 127.0.0.1:4000 -k "passwd" --raw-mode faketcp  >> /var/log/udp2raw.log &
+
+# WireGuard + UDP2RAW 伪装 TCP  预留端口  8866
+udp2raw -s -l0.0.0.0:8866 -r 127.0.0.1:9009 -k "passwd" --raw-mode faketcp  >> /var/log/wg_udp2raw.log &
 
 exit 0
 EOF
