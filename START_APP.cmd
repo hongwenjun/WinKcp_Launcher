16806b713fc0ebdbb1a49e12bd46d9cf75b9aaf6
蘭雅sRGB <hongwenjun@gmail.com>
2018-11-18 21:43:25 +0800

    Add files via upload

--- START_APP.cmd
+++ START_APP.cmd
@@ -4,8 +4,8 @@
 ::  两个冒号是注释，删除命令行首开启命令
 
 ::  运行 WireGuard 客户端 TunSafe
-    CD /D  C:\soft\TunSafe\
-    start TunSafe.exe
+::  CD /D  C:\soft\TunSafe\
+::  start TunSafe.exe
 
 ::  运行 Shadowsocks 客户端
 ::  CD /D  C:\soft\Shadowsocks
@@ -18,3 +18,10 @@
 ::  set PORT=1234
 ::  CD /D  C:\soft\Brook
 ::  start /b  brook client -l 127.0.0.1:2080 -i 127.0.0.1 -s %IP_URL%:%PORT% -p %PASSWORD%
+
+
+@set SERVER_IP=144.202.95.95
+
+@route add %SERVER_IP% mask 255.255.255.0 default METRIC default IF default
+
+@start /b speederv2 -c -l0.0.0.0:9009 -r%SERVER_IP%:8888 -f20:10 -k "passwd" --mode 0
---
+++ UDPspeeder.cmd
@@ -0,0 +1,8 @@
+@echo.
+@set PATH=%~dp0;%PATH%
+
+@set SERVER_IP=144.202.95.95
+
+@route add %SERVER_IP% mask 255.255.255.0 default METRIC default IF default
+
+@start /b speederv2 -c -l0.0.0.0:9009 -r%SERVER_IP%:8888 -f20:10 -k "passwd" --mode 0
