060b14f97fb7bd8b257bc25b859aacad987f3d7c
蘭雅sRGB <hongwenjun@gmail.com>
2018-12-02 17:15:47 +0800

    Update wg_udp2raw.sh

--- wg_udp2raw.sh
+++ wg_udp2raw.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# 修改ss-server密码，SS默认不能直接访问，修改第26行  ss-server -s 0.0.0.0 (把原来127.0.0.1 改成4个0)
-PASSWORD=srgb.xyz
+# 密码随机
+str=$(date | md5sum  | awk '{print $1}')
+passwd=${str:0:8}
 
-# 停止原先服务
-systemctl stop rc-local
+# 端口参数
+wg_port=$(wg | grep 'listening port:' | awk '{print $3}')
+raw_port=`expr $wg_port + 1000`
+serverip=$(curl -4 icanhazip.com)
+==================================================
+# 定义文字颜色
+Green="\033[32m"  && Red="\033[31m" && GreenBG="\033[42;37m" && RedBG="\033[41;37m" && Font="\033[0m"
 
 # 下载 UDP2RAW
 wget https://github.com/wangyu-/udp2raw-tunnel/releases/download/20181113.0/udp2raw_binaries.tar.gz
@@ -13,15 +19,6 @@ mv udp2raw_amd64 /usr/bin/udp2raw
 rm udp2raw* -rf
 rm version.txt
 
-# 下载 UDPspeeder
-wget https://github.com/wangyu-/UDPspeeder/releases/download/20180806.0/speederv2_linux.tar.gz
-tar xf speederv2_linux.tar.gz
-mv speederv2_amd64 /usr/bin/speederv2
-rm speederv2* -rf
-rm version.txt
-
-
-
 #安装到启动项 适合Debian 9  Ubuntu 18
 
 cat <<EOF >/etc/rc.local
@@ -29,22 +26,20 @@ cat <<EOF >/etc/rc.local
 #
 # rc.local
 
-# SS+KCP+UDP2RAW 加速  端口  8855
-ss-server -s 127.0.0.1 -p 40000 -k ${PASSWORD} -m aes-256-gcm -t 300 >> /var/log/ss-server.log &
-kcp-server -t "127.0.0.1:40000" -l ":4000" -mode fast2 -mtu 1300  >> /var/log/kcp-server.log &
-udp2raw -s -l0.0.0.0:8855 -r 127.0.0.1:4000 -k "passwd" --raw-mode faketcp  >> /var/log/udp2raw.log &
+# WireGuard + UDP2RAW 伪装 TCP  原端口 数值加 1000
+udp2raw -s -l0.0.0.0:${raw_port} -r 127.0.0.1:${wg_port} -k '${passwd}' --raw-mode faketcp  >> /var/log/wg_udp2raw.log &
 
-# WireGuard + UDP2RAW 伪装 TCP  端口  8866
-udp2raw -s -l0.0.0.0:8866 -r 127.0.0.1:9009 -k "passwd" --raw-mode faketcp  >> /var/log/wg_udp2raw.log &
-
-# WireGuard + UDPspeeder 加速UDP TCP  端口  8888
-speederv2 -s -l0.0.0.0:8888 -r127.0.0.1:9009  -f20:10 -k "passwd" --mode 0  >> /var/log/speederv2.log &
+#  Windows 端 UDP2RAW 转接命令行
+#  start /b udp2raw -c -r${serverip}:${raw_port} -l0.0.0.0:${wg_port} -k '${passwd}' --raw-mode faketcp 
 
 exit 0
 EOF
 
+systemctl stop rc-local
 chmod +x /etc/rc.local
 systemctl restart rc-local
-
 cat /etc/rc.local
-cat /etc/wireguard/client.conf
+
+echo -e "${GreenBG}   WireGuard 原端口: ${wg_port} ;   UDP2RAW伪装TCP后端口: ${raw_port} ; 转发密码: ${passwd} ${Font}"
+echo     "#  Windows 端 UDP2RAW 转接命令行"
+echo -e "${Green}  start /b udp2raw -c -r${serverip}:${raw_port} -l0.0.0.0:${wg_port} -k '${passwd}' --raw-mode faketcp  ${Font}"
