af6272883409c363e211d2743959c32afd4efd07
蘭雅sRGB <hongwenjun@gmail.com>
2019-01-02 21:51:49 +0800

    Update START_APP.cmd

--- START_APP.cmd
+++ START_APP.cmd
@@ -1,21 +1,31 @@
 @echo.
 @set PATH=%~dp0;%PATH%
 
-::  Á½¸öÃ°ºÅÊÇ×¢ÊÍ£¬É¾³ıÃüÁîĞĞÊ×¿ªÆôÃüÁî
+::  ä¸¤ä¸ªå†’å·æ˜¯æ³¨é‡Šï¼Œåˆ é™¤å‘½ä»¤è¡Œé¦–å¼€å¯å‘½ä»¤
 
-::  ÔËĞĞ WireGuard ¿Í»§¶Ë TunSafe
+::  è¿è¡Œ WireGuard å®¢æˆ·ç«¯ TunSafe
 ::  CD /D  C:\soft\TunSafe\
 ::  start TunSafe.exe
 
-::  ÔËĞĞ Shadowsocks ¿Í»§¶Ë
+::  è¿è¡Œ Shadowsocks å®¢æˆ·ç«¯
 ::  CD /D  C:\soft\Shadowsocks
 ::  start Shadowsocks.exe
 
+::  BROOK + KcpTun + Udp2Raw å®¢æˆ·ç«¯æ¥å£é…ç½®
+@set SERVER_IP=1.2.3.4
+@set PORT=2999
+@set PASSWORD=wg2999
+@set BK_PORT=3999
 
-::  ´ò¿ª Brook ¿Í»§¶Ë·şÎñ
-@set IP_URL=1.2.3.4
-@set PASSWORD=brook3999
-@set PORT=3999
+@start /b kcp-client -r "127.0.0.1:4000" -l ":%BK_PORT%" --key %PASSWORD% -mode fast2 -mtu 1300
+@start /b udp2raw -c -r%SERVER_IP%:%PORT% -l0.0.0.0:4000 -k %PASSWORD% --raw-mode faketcp
 
-::  CD /D  C:\soft\Brook
-::  start /b  brook client -l 127.0.0.1:2080 -i 127.0.0.1 -s %IP_URL%:%PORT% -p %PASSWORD%
+
+::  æ‰“å¼€ Brook å®¢æˆ·ç«¯æœåŠ¡
+set IP_URL=127.0.0.1
+set PASSWORD=wg2999
+set PORT=3999
+
+CD /D  C:\soft\Brook
+start /b  brook client -l 127.0.0.1:2080 -i 127.0.0.1 -s %IP_URL%:%PORT% -p %PASSWORD%
+
